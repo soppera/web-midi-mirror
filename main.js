5e1344f1c1f51663dcfce94ab9b024783063fb4d
soppera <51345626+soppera@users.noreply.github.com>
2019-06-04 01:51:57 +0200

    First commit.

---
+++ main.js
@@ -0,0 +1,107 @@
+"use strict";
+
+(function() {
+  function Filter(in_port, out_port) {
+    this.in_port = in_port;
+    this.out_port = out_port;
+    this.listener = null;
+  }
+
+  Filter.prototype.connect = function connect() {
+    if (this.listener !== null) {
+      throw new Error("already connected")
+    }
+    this.listener = this.onmidimessage.bind(this);
+    this.in_port.addEventListener("midimessage", this.listener);
+  };
+
+  Filter.prototype.disconnect = function disconnect() {
+    if (this.listener === null) {
+      throw new Error("not connected")
+    }
+    this.in_port.removeEventListener("midimessage", this.listener);
+    this.listener = null;
+  };
+
+  Filter.prototype.onmidimessage = function onmidimessage(e) {
+    if (this.listener === null) {
+      throw new Error("disconnected");
+    }
+    let data = e.data;
+    switch (data[0] & 0xf0) {
+      case 0x90:		// note on
+      case 0x80:		// note off
+	data = new Uint8Array(data);
+	let n = 124 - data[1];
+	if (n < 0) {
+	  console.log("ignoring out of range note:", n,
+		      " (created from", data[1], ")");
+	  return;
+	}
+	data[1] = n;
+	break;
+    }
+    this.out_port.send(data);
+  };
+
+  function midi_port_json(p) {
+    return {
+      id: p.id,
+      manufacturer: p.manufacturer,
+      name: p.name,
+      type: p.type,
+    }
+  }
+
+  function fill_port_selector(selector, ports_map, local_storage_key) {
+    for (let [k, p] of ports_map) {
+      let opt = document.createElement("option");
+      selector.appendChild(opt);
+      opt.value = k;
+      opt.innerText = p.name;
+    }
+    let pref = localStorage.getItem(local_storage_key);
+    if (pref !== null && ports_map.has(pref)) {
+      selector.value = pref;
+    }
+  }
+
+  var filter = null;
+  var inputs = null;
+  var outputs = null;
+  var input_sel = document.getElementById("input-sel");
+  var output_sel = document.getElementById("output-sel");
+  const input_port_key = "input_port";
+  const output_port_key = "output_port";
+
+  function onselchange() {
+    if (filter !== null) {
+      filter.disconnect();
+      filter = null;
+    }
+
+    localStorage.setItem(input_port_key, input_sel.value);
+    localStorage.setItem(output_port_key, output_sel.value);
+
+    filter = new Filter(inputs.get(input_sel.value), outputs.get(output_sel.value))
+    filter.connect();
+  }
+
+  navigator.requestMIDIAccess()
+      .then(function(access) {
+	inputs = access.inputs;
+	outputs = access.outputs;
+
+	fill_port_selector(input_sel, access.inputs, input_port_key);
+	fill_port_selector(output_sel, access.outputs, output_port_key);
+
+	onselchange()
+	input_sel.addEventListener("change", onselchange);
+	output_sel.addEventListener("change", onselchange);
+
+	access.onstatechange = function(e) {
+	  console.log("state change:", e.port.name, e.port.manufacturer, e.port.state);
+	};
+      });
+
+})()
